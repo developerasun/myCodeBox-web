0d4034c18a3f784e4a41ad603933d3090629439e
designerasun <asuntheawesome@gmail.com>
2022-04-01 15:09:59 +0900

    feat: embedding simmer io tag to svelte comp

---
+++ myJavascript/frontEnd/mySvelte/first-app/src/components/Game.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+<!-- import jumping car game from simmer.io -->
+<iframe 
+    title="jumping car"
+    src="https://i.simmer.io/@developerasun/jumpingcar2" 
+    style="width:960px;height:600px;border:0">
+</iframe>
+
+</main>
+
+<style>
+
+</style>
