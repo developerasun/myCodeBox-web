151aaa2c7a9b1890debd6d971a611edbe262d715
designerasun <asuntheawesome@gmail.com>
2022-04-01 15:11:22 +0900

    feat: adding prop to svelte component

---
+++ myJavascript/frontEnd/mySvelte/first-app/src/components/ModalChildren.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+export let name; // component prop
+</script>
+
+<main id="modalChildren">
+    From modal child : Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ipsum?
+</main>
+
+<style lang="scss">
+#modalChildren {
+    color: blueviolet;
+}
+</style>
