a7ac6f21c8e00719ffcdac9c3aff79aada4126cc
designerasun <asuntheawesome@gmail.com>
2022-04-01 15:10:36 +0900

    feat: add modal with slot and event modifier

--- myJavascript/frontEnd/mySvelte/first-app/src/components/Modal.svelte
+++ myJavascript/frontEnd/mySvelte/first-app/src/components/Modal.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 // create a prop for this component
 export let message;
 
@@ -11,18 +11,26 @@ const handleToggle = () => {
 
 <div id="modal">
     <!-- event forwarding Svelte -->
-    <div id="container" class:promo={true} on:click={handleToggle}>
+    <!-- once : event modifier -->
+    <div id="container" class:promo={true} on:click|once ={handleToggle}>
         <p>sign up for play</p>
         {#if toggle === true}
             {message}
             {:else}
             "not toggled"
         {/if}
+        <slot name="greetings"></slot>
     </div>
+    <!-- Components can have child content with slot tag, in the same way that elements can. -->
+    <div id="secondModalChild">
+        <slot></slot>
+    </div>
+    <!-- named slot -->
+    
 </div>
 
 
-<style>
+<style lang="scss">
 #container { 
     background-color: rgb(223, 223, 223);
     padding: 2rem;
@@ -31,6 +39,11 @@ const handleToggle = () => {
 
 .promo { 
     box-shadow: 5px 5px 10px red;
+}
+
+#secondModalChild {
+    background-color: blue;
+    font-size: 1.2rem;
 }
 
 </style>
