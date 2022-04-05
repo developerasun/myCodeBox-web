4a002accce89c0273174f9b52a0221a961015214
designerasun <asuntheawesome@gmail.com>
2022-04-05 17:27:16 +0900

    feat :update store value with writable.update, reactive $

---
+++ myJavascript/frontEnd/mySvelte/first-app/src/components/CheckStore.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { onMount, onDestroy } from 'svelte';
+
+import app from '../main';
+
+    import {count} from '../store/store'
+
+    const increase = () => {
+        count.update(num => num +1)
+    }
+    const decrease = () => {
+        count.update(num => num -1)
+    }
+
+</script>
+
+<main>
+    <!-- Any name beginning with $ is assumed to refer to a store value. -->
+    current store value: {$count}
+    
+    <button on:click={increase}>+</button>
+    <button on:click={decrease}>-</button>
+</main>
