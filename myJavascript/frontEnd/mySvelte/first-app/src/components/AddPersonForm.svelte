2d4274d0877e199d3a1b769c46365b1317e4239a
designerasun <asuntheawesome@gmail.com>
2022-04-05 14:46:32 +0900

    feat : bidirectional data flow with value binding, event forwarding added

---
+++ myJavascript/frontEnd/mySvelte/first-app/src/components/AddPersonForm.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+let nation
+let sex
+let age
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(nation, sex, age)
+}
+</script>
+
+
+
+<main>
+    <!-- event forwarding -->
+    <form action="" on:submit={handleSubmit} on:click> 
+        <!-- value binding -->
+        <input type="text" name="" id="" bind:value={nation}>
+        <input type="text" name="" id="" bind:value={sex}>
+        <input type="number" name="" id="" bind:value={age}>
+        <button>submit</button>
+    </form>
+</main>
+
+
+
+<style lang="scss"></style>
