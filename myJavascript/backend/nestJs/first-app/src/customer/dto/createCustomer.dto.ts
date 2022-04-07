553c6c3fab1afc3a6eb485bdd6d74a0772821c17
designerasun <asuntheawesome@gmail.com>
2022-04-07 12:45:03 +0900

    feat:add class-validator for data validation

--- myJavascript/backend/nestJs/first-app/src/customer/dto/createCustomer.dto.ts
+++ myJavascript/backend/nestJs/first-app/src/customer/dto/createCustomer.dto.ts
@@ -1,6 +1,21 @@
+import { Type } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsNumberString, ValidateNested } from "class-validator";
+import { CreateAddressDto } from "./createAddress.dto";
+
 export class CreateCustomerDto {
-    // body
+    // request body
+
+    @IsNumberString()
     id:number ;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty() // not isNotEmpty
     name : string;
+
+    @IsNotEmptyObject()
+    @ValidateNested()
+    @Type(()=>CreateAddressDto)
+    address : CreateAddressDto
 }
