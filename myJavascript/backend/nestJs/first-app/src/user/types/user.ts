5dfe593857b81921dd55b861f62c29cf49fcea0f
designerasun <asuntheawesome@gmail.com>
2022-04-08 10:43:53 +0900

    feat: serializing data with constructor, TS partial

---
+++ myJavascript/backend/nestJs/first-app/src/user/types/user.ts
@@ -0,0 +1,17 @@
+import { Exclude } from "class-transformer"
+
+export interface User {
+    username: string
+    password: string
+}
+
+export class SerializeUser {
+    username: string
+
+    @Exclude() // Marks the given class or property as excluded
+    password:string;
+
+    constructor(partial: Partial<SerializeUser>){
+        Object.assign(this, partial)
+    }
+}
