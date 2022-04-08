87004031e111af3f83e76e461084d32967293ce9
designerasun <asuntheawesome@gmail.com>
2022-04-08 12:36:50 +0900

    feat: apply middleware to module level

--- myJavascript/backend/nestJs/first-app/src/customer/customer.module.ts
+++ myJavascript/backend/nestJs/first-app/src/customer/customer.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { NextFunction } from 'express';
 import { CustomerController } from './controller/customer/customer.controller';
+import { ValidateCustomerMiddleware } from './middleware/validate-customer.middleware copy';
+import { ValidateExpiredCustomerMiddleware } from './middleware/validate-expired-customer.middleware';
 import { CustomerService } from './service/customer/customer.service';
 
 @Module({
@@ -8,4 +11,24 @@ import { CustomerService } from './service/customer/customer.service';
     providers:[CustomerService]
 })
 
-export class CustomerModule {}
+export class CustomerModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        // middleware class/function or array of classes/functions to be attached to the passed routes
+        consumer.apply(
+            ValidateCustomerMiddleware, 
+            ValidateExpiredCustomerMiddleware, 
+            (req:Request, res:Response, next:NextFunction) => {
+                console.log("last middleware")
+                next() // should be called to move on
+            }
+        ).forRoutes(
+        {
+            path: 'customer/search/:id',
+            method: RequestMethod.GET
+        },
+        {
+            path: 'customer/:id', 
+            method: RequestMethod.GET
+        })
+    }
+}
