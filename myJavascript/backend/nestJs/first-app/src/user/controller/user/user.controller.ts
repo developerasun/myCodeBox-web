1ea3a5f9e710a3c179bf8e3706a7b4937e0d9b9a
designerasun <asuntheawesome@gmail.com>
2022-04-11 06:15:23 +0900

    feat: UseInterceptors, UseFilter, built-in nest js http exception added

--- myJavascript/backend/nestJs/first-app/src/user/controller/user/user.controller.ts
+++ myJavascript/backend/nestJs/first-app/src/user/controller/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { ClassSerializerInterceptor, Controller, Get, HttpException, HttpStatus, Param, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, HttpException, HttpStatus, NotFoundException, Param, ParseIntPipe, UseFilters, UseInterceptors } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/user/filter/HttpException.filter';
 import { UserService } from 'src/user/service/user/user.service';
 import { SerializeUser } from 'src/user/types/user';
 
@@ -17,7 +18,20 @@ export class UserController {
     @Get('/:username')
     getByUsername(@Param('username') username: string) {
         const user = this.userService.getUserByUsername(username)
+
+        // return Serialized user to hide password information
+        // if user returned, the password is shown
         if (user) return new SerializeUser(user)
         else throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    }
+
+    // @UserInterceptors: Decorator that binds interceptors to the scope of the controller or method, depending on its context.
+    @UseInterceptors(ClassSerializerInterceptor)
+    @UseFilters(HttpExceptionFilter)
+    @Get('searchID/:id')
+    getByUserId(@Param('id', ParseIntPipe) id: number) {
+        const user = this.userService.getUserById(id)
+        if (user) return new SerializeUser(user)
+        else throw new NotFoundException() // Nest js built-in HttpException
     }
 }
