f4045dbbb3e62d5198a6538953b1000bfcefe91e
designerasun <asuntheawesome@gmail.com>
2022-06-29 01:10:18 +0900

    feat: add module side effect example

---
+++ myJavascript/basics/sideEffect/a.js
@@ -0,0 +1,11 @@
+//a.js
+export function print1() {
+  console.log("export print1 is working");
+}
+
+export function print2() {
+  console.log("non-export print2 is working");
+}
+
+print1();
+print2();
---
+++ myJavascript/basics/sideEffect/b.js
@@ -0,0 +1,3 @@
+//b.js
+import { print1, print2 } from "./a.js"; // good to go if the print1,2 are all exported
+// import "./a.js"; // import only to initialize. this will throw module not found error
