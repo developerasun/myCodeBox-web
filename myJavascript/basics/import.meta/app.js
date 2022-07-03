a8f4f3ecececac68029f96e759074393f1b885c5
designerasun <asuntheawesome@gmail.com>
2022-07-03 18:13:48 +0900

    feat: add chalk module for terminal formating

--- myJavascript/basics/import.meta/app.js
+++ myJavascript/basics/import.meta/app.js
@@ -1,2 +1,6 @@
+import chalk from "chalk";
 console.log(import.meta);
+
+console.log(chalk.green.underline("DONE"));
+console.log(chalk.red.bold("RED PREFIX IN TERMINAL"));
 console.log(import.meta.url);
