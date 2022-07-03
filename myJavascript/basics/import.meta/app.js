1790d3754701d27c15b361df540695848b588b70
designerasun <asuntheawesome@gmail.com>
2022-07-03 22:03:07 +0900

    feat: add custom filename, dirname method in ESM

--- myJavascript/basics/import.meta/app.js
+++ myJavascript/basics/import.meta/app.js
@@ -4,3 +4,12 @@ console.log(import.meta);
 console.log(chalk.green.underline("DONE"));
 console.log(chalk.red.bold("RED PREFIX IN TERMINAL"));
 console.log(import.meta.url);
+
+const filename = import.meta.url.split("/").pop(); // __filename is not defined in ESM
+const dirname = import.meta.url.split("/").slice(3, -1).join("/"); // __dirname is not defined in ESM
+
+console.log(filename);
+console.log(dirname);
+
+// console.log(__dirname); // error
+// console.log(__filename); // error
