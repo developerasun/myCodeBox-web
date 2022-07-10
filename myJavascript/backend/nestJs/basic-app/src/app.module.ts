b276644178c4481564579b743e503f7fd8f18a30
designerasun <asuntheawesome@gmail.com>
2022-07-10 23:15:17 +0900

    feat: add the separate module to application module

--- myJavascript/backend/nestJs/basic-app/src/app.module.ts
+++ myJavascript/backend/nestJs/basic-app/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { QuizcontrollerController } from './modules/quiz/quizcontroller.controller';
 
 @Module({
   imports: [QuizModule],
-  controllers: [AppController, QuizcontrollerController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
