707c90d32754a25f46d75a1d152ff8b5673d79c7
designerasun <asuntheawesome@gmail.com>
2022-07-10 23:14:41 +0900

    feat: set a separate module

--- myJavascript/backend/nestJs/basic-app/src/modules/quiz/quiz.module.ts
+++ myJavascript/backend/nestJs/basic-app/src/modules/quiz/quiz.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { QuizService } from './quiz.service';
 import { QuizcontrollerController } from './quizcontroller.controller';
 
 @Module({
+  providers: [QuizService],
   controllers: [QuizcontrollerController],
 })
 export class QuizModule {}
