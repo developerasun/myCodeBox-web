13b734887b0929d189a9f7217f72a99aa2836763
designerasun <asuntheawesome@gmail.com>
2022-07-10 23:29:38 +0900

    feat: add the string getter module to app module

--- myJavascript/backend/nestJs/basic-app/src/app.module.ts
+++ myJavascript/backend/nestJs/basic-app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
+import { ReturnNameModule } from './modules/return-name/return-name.module';
 
 @Module({
-  imports: [QuizModule],
+  imports: [QuizModule, ReturnNameModule],
   controllers: [AppController],
   providers: [AppService],
 })
