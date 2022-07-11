fe356726b6d3586c8463205c0010f1d53b86be44
designerasun <asuntheawesome@gmail.com>
2022-07-12 01:06:49 +0900

    feat: load typeorm module to application module

--- myJavascript/backend/nestJs/basic-app/src/app.module.ts
+++ myJavascript/backend/nestJs/basic-app/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
+import { typeormOptions } from './config/typeorm.config';
 import { ReturnNameModule } from './modules/return-name/return-name.module';
 
 @Module({
-  imports: [QuizModule, ReturnNameModule],
+  imports: [
+    QuizModule,
+    ReturnNameModule,
+    TypeOrmModule.forRoot(typeormOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
